64da9bee2dcf52bb1c39f0abbb0dfa3a4099c5a2
emmanuelmermur.99@gmail.com <emmanuelmercado@192.168.1.19>
2024-10-20 23:55:21 -0600

    Se da estilo a los componentes de Login y SignUp

--- FrontEnd/innovatube/src/components/Login/Login.jsx
+++ FrontEnd/innovatube/src/components/Login/Login.jsx
@@ -4,11 +4,19 @@ import "./Login.css"
 
 const Login = () => {
     return (
+        
         <div className='loginBody'>
-            <h1>Componente del login</h1>
-            <p>
-        ¿No tienes una cuenta? <Link to="/signup">Crear cuenta</Link>
-            </p>
+            <div class="bg"></div>
+            <div class="bg bg2"></div>
+            <div class="bg bg3"></div>
+            <div className='loginBox'>
+            <img className="logo" src="castor3.png" alt="logo" />
+            <input type="email" class="form-control inputText" id="username" placeholder="Ingrese su usuario o su correo"></input>
+            <input type="password" class="form-control inputText" id="password" placeholder="Ingrese su contraseña"></input>
+            <button type="button" class="btn btnSubmit">Ingresar</button>
+            <p>¿No tienes una cuenta? <Link to="/signup">Crear cuenta</Link></p>
+            </div>
+            
         </div>
     )
 }
--- FrontEnd/innovatube/src/components/SignUp/SignUp.jsx
+++ FrontEnd/innovatube/src/components/SignUp/SignUp.jsx
@@ -1,9 +1,28 @@
 
+import { Link } from 'react-router-dom';
+import "./SignUp.css"
 
 const SignUp = () =>{
     return(
-        <div>
-        <h1>Componente del registro</h1>
+        <div className='signUpBody'>
+            <div class="bg"></div>
+            <div class="bg bg2"></div>
+            <div class="bg bg3"></div>
+
+
+            <div className='signUpBox'>
+            <img className="logo" src="castor3.png" alt="logo" />
+            <input type="input" class="form-control inputText" id="name" placeholder="Nombre"></input>
+            <input type="input" class="form-control inputText" id="lastname" placeholder="Apellidos"></input>
+            <input type="input" class="form-control inputText" id="user" placeholder="Usuario"></input>
+            <input type="password" class="form-control inputText" id="password" placeholder="Ingrese su contraseña"></input>
+            <input type="password" class="form-control inputText" id="password" placeholder="Reingrese su contraseña"></input>
+
+            <button type="button" class="btn btnSubmit">Crear</button>
+            <p>¿Ya tienes una cuenta?</p>
+            <p>Ingresa aquí <Link to="/">Iniciar sesión</Link></p>
+            </div>
+            
         </div>
     )
 }
