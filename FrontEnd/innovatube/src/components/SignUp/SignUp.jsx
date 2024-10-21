9950a01d68f3da5460aefc70f5e221cd6b9fad8a
emmanuelmermur.99@gmail.com <emmanuelmercado@192.168.1.19>
2024-10-21 08:02:37 -0600

    Se agrega funcionalidad al SignUp, en proceso de validar el funcionamiento de Recaptcha

--- FrontEnd/innovatube/src/components/SignUp/SignUp.jsx
+++ FrontEnd/innovatube/src/components/SignUp/SignUp.jsx
@@ -1,28 +1,95 @@
-
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./SignUp.css"
+import ReCAPTCHA from 'react-google-recaptcha';
+import CryptoJS from 'crypto-js';
+import { addUser } from '../../redux/slices/userSlices';
+import { useDispatch, useSelector } from 'react-redux';
 
-const SignUp = () =>{
-    return(
+
+
+
+const SignUp = () => {
+    const dispatch = useDispatch();
+    const users = useSelector((state) => state.userData.users)
+    const [formData, setFormData] = useState({
+        name: "",
+        lastname: "",
+        user: "",
+        password: "",
+    })
+
+    const [passwordOk, setPasswordOk] = useState({
+        password2: ""
+    })
+
+    const [captcha, setCaptcha] = useState(null)
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+    }
+    const handleInputChangePassword = (e) => {
+        const { name, value } = e.target
+        setPasswordOk({ ...passwordOk, [name]: value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (formData.password === passwordOk.password2) {
+            console.log('Si es la misma');
+            const userExists = users.some(existingUser => existingUser.user === formData.user);
+
+            if (userExists) {
+                alert("Ya existe este usuario, favor de ingresar otro")
+            }
+            else {
+                const hashedPassword = CryptoJS.MD5(formData.password).toString()
+                alert("Usuario agregado correctamente")
+                dispatch(addUser({ ...formData, password: hashedPassword }))
+                setFormData({
+                    name: "",
+                    lastname: "",
+                    user: "",
+                    password: "",
+                })
+                setPasswordOk({
+                    password2:""
+                })
+            }
+        }
+        else {
+
+        }
+    }
+
+
+    return (
         <div className='signUpBody'>
             <div class="bg"></div>
             <div class="bg bg2"></div>
             <div class="bg bg3"></div>
 
-
             <div className='signUpBox'>
-            <img className="logo" src="castor3.png" alt="logo" />
-            <input type="input" class="form-control inputText" id="name" placeholder="Nombre"></input>
-            <input type="input" class="form-control inputText" id="lastname" placeholder="Apellidos"></input>
-            <input type="input" class="form-control inputText" id="user" placeholder="Usuario"></input>
-            <input type="password" class="form-control inputText" id="password" placeholder="Ingrese su contraseña"></input>
-            <input type="password" class="form-control inputText" id="password" placeholder="Reingrese su contraseña"></input>
+                <img className="logo" src="castor3.png" alt="logo" />
 
-            <button type="button" class="btn btnSubmit">Crear</button>
-            <p>¿Ya tienes una cuenta?</p>
-            <p>Ingresa aquí <Link to="/">Iniciar sesión</Link></p>
+                <form onSubmit={handleSubmit} className='signUpForm'>
+                    <input type="input" class="form-control inputText" value={formData.name} onChange={handleInputChange} id="name" name="name" placeholder="Nombre"></input>
+                    <input type="input" class="form-control inputText" value={formData.lastname} onChange={handleInputChange} id="lastname" name="lastname" placeholder="Apellidos"></input>
+                    <input type="input" class="form-control inputText" value={formData.user} onChange={handleInputChange} id="user" name="user" placeholder="Usuario"></input>
+                    <input type="password" class="form-control inputText" value={formData.password} onChange={handleInputChange} id="password" name="password" placeholder="Ingrese su contraseña"></input>
+                    <input type="password" class="form-control inputText" value={passwordOk.password2} onChange={handleInputChangePassword} id="password2" name="password2" placeholder="Reingrese su contraseña"></input>
+                    <ReCAPTCHA
+                        sitekey="6LfMnWMqAAAAAJ6r1fXFv6pSSOt92f-cnVGU6ao5" 
+                        onChange={(value) => setCaptcha(value)} 
+                    />
+                    <button type="submit" class="btn btnSubmit">Crear</button>
+                </form>
+
+                <p>¿Ya tienes una cuenta?</p>
+                <p>Ingresa aquí <Link to="/">Iniciar sesión</Link></p>
             </div>
-            
+
         </div>
     )
 }
--- FrontEnd/innovatube/src/redux/store.js
+++ FrontEnd/innovatube/src/redux/store.js
@@ -1,10 +1,12 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './slices/globalStates';
+import userReducer from './slices/userSlices';
 
 const store = configureStore({
   reducer: {
     loginSession: loginReducer,
+    userData: userReducer
   },
 });
 
